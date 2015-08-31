0e1d3a9b5116802c2daa23a76a9c0c85e8257eb8
rag <rag@pi>
2015-08-31 15:22:25 +0000

    storing

--- oscar/settings.py
+++ oscar/settings.py
@@ -110,7 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '/home/pi/oscar/static'
 
 #Celery Settings
-BROKER_URL = 'amqp://oscar:oscar@10.0.0.128:5672/oscar'
+#BROKER_URL = 'amqp://oscar:oscar@10.0.0.128:5672/oscar'
+BROKER_URL = 'amqp://oscar:oscar@127.0.0.1:5672/oscar'

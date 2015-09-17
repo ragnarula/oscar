206d8e5f596605caf056f4f15a424541482100f8
Raghav Narula <ragnarula87@googlemail.com>
2015-09-17 11:04:25 +0200

    wsgi

--- oscar/wsgi.py
+++ oscar/wsgi.py
@@ -14,5 +14,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oscar.settings")
-application = wsgi.WSGIHandler()
-# application = get_wsgi_application()
+application = get_wsgi_application()

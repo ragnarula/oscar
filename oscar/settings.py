73818cc05269bf5f6ba3012c905ffd0d2fa8a0ad
Raghav Narula <ragnarula87@googlemail.com>
2015-09-18 09:08:14 +0200

    merged dev branch

--- oscar/settings.py
+++ oscar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7r2=sw#x26=*8e&4fl#m-p*+hy596=7m_165+z_om*3_jm1(%='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
